1344ff52d58f9d33c7615227c905270eae12ccc1
Arnaud Bosc <arnaud.bosc@elium.tv>
2016-06-21 10:06:26 +0200

    feat: add cjs, umd and es6 module generation

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from "rollup-plugin-typescript";
+import buble from "rollup-plugin-buble";
+
+
+export default {
+  entry: 'src/index.ts',
+  external: ["lodash"],
+  plugins: [
+    typescript({tsconfig: false}),
+    buble()
+  ],
+  targets: [
+    {dest: 'lib/mighty-js.cjs.js', format: 'cjs', sourceMap: true},
+    {dest: 'lib/mighty-js.umd.js', format: 'umd', sourceMap: true, moduleName: "mighty-js"},
+    {dest: 'lib/mighty-js.es6.js', format: 'es', sourceMap: true}
+  ]
+}
